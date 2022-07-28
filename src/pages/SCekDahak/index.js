b6a0d9383ba3e466acac9aa2a42e5b8af8cae59d
Fachreza Maulana <zzafachreza@gmail.com>
2022-07-28 16:13:35 +0700

    alhamdulillah

--- src/pages/SCekDahak/index.js
+++ src/pages/SCekDahak/index.js
@@ -5,7 +5,7 @@ import axios from 'axios';
 import { colors, fonts, windowWidth } from '../../utils';
 import { MyButton, MyGap, MyInput } from '../../components';
 import { apiURL } from '../../utils/localStorage';
-
+import { launchCamera, launchImageLibrary } from 'react-native-image-picker';
 
 export default function SCekDahak({ navigation, route }) {
 
@@ -39,6 +39,125 @@ export default function SCekDahak({ navigation, route }) {
 
 
 
+    const [foto, setfoto] = useState('https://zavalabs.com/nogambar.jpg');
+
+    const options = {
+        includeBase64: true,
+        quality: 0.3,
+    };
+
+    const getCamera = xyz => {
+        launchCamera(options, response => {
+            // console.log('Response = ', response);
+            if (response.didCancel) {
+                console.log('User cancelled image picker');
+            } else if (response.error) {
+                console.log('Image Picker Error: ', response.error);
+            } else {
+                let source = { uri: response.uri };
+                switch (xyz) {
+                    case 1:
+                        setfoto(`data:${response.type};base64, ${response.base64}`)
+                        break;
+                }
+            }
+        });
+    };
+
+    const getGallery = xyz => {
+        launchImageLibrary(options, response => {
+            console.log('All Response = ', response);
+
+            console.log('Ukuran = ', response.fileSize);
+            if (response.didCancel) {
+                console.log('User cancelled image picker');
+            } else if (response.error) {
+                console.log('Image Picker Error: ', response.error);
+            } else {
+                if (response.fileSize <= 200000) {
+                    let source = { uri: response.uri };
+                    switch (xyz) {
+                        case 1:
+                            setfoto(`data:${response.type};base64, ${response.base64}`)
+                            break;
+                    }
+                } else {
+                    Alert.alert('Dement Tomat', 'Ukuran Foto Terlalu Besar Max 500 KB')
+                }
+            }
+        });
+    };
+
+    const UploadFoto = ({ onPress1, onPress2, label, foto }) => {
+        return (
+            <View
+                style={{
+                    padding: 10,
+                    backgroundColor: colors.white,
+                    marginVertical: 10,
+                    borderWidth: 1,
+                    borderRadius: 10,
+                    borderColor: colors.border,
+                    elevation: 2,
+                }}>
+                <Text
+                    style={{
+                        fontFamily: fonts.secondary[600],
+                        color: colors.black,
+                    }}>
+                    {label}
+                </Text>
+
+                <View
+                    style={{
+                    }}>
+
+                    <Image
+                        source={{
+                            uri: foto,
+                        }}
+                        style={{
+                            width: '100%',
+                            aspectRatio: 2,
+                            resizeMode: 'contain',
+                        }}
+                    />
+
+                    <View style={{
+                        flexDirection: 'row'
+                    }}>
+                        <View
+                            style={{
+                                flex: 1,
+                                paddingRight: 5,
+                            }}>
+                            <MyButton
+                                onPress={onPress1}
+                                colorText={colors.white}
+                                title="KAMERA"
+                                warna={colors.primary}
+                            />
+                        </View>
+                        <View
+                            style={{
+                                flex: 1,
+                                paddingLeft: 5,
+                            }}>
+                            <MyButton
+                                onPress={onPress2}
+                                title="GALLERY"
+                                colorText={colors.white}
+                                warna={colors.secondary}
+                            />
+                        </View>
+                    </View>
+
+
+
+                </View>
+            </View>
+        );
+    };
     return (
         <SafeAreaView style={{
 
@@ -61,21 +180,25 @@ export default function SCekDahak({ navigation, route }) {
                     padding: 10,
                 }}>
 
-                    <MyInput value={bta} onChangeText={x => setBta(x)} label="BTA" iconname="search" placeholder="Masukan nilai BTA" />
+                    <UploadFoto
+                        onPress1={() => getCamera(1)}
+                        onPress2={() => getGallery(1)}
+                        label="Upload Bukti Foto"
+                        foto={foto}
+                    />
                     <MyGap jarak={20} />
                     <MyButton onPress={() => {
 
 
 
-                        if (bta.length == 0) {
+                        if (foto.length == 0) {
                             Alert.alert('Informasi Demen Tomat', 'BTA Wajib di isi !');
                         } else {
                             axios.post(apiURL + 'update_pengobatan.php', {
                                 nik_ktp: item.nik_ktp,
-                                status_keluarga: 'Dalam Pengobatan',
-                                bta: bta
+                                bta: foto
                             }).then(res => {
-                                Alert.alert('Demen Tomat', 'Berhasil disimpan !')
+                                Alert.alert('Demen Tomat', 'Menunggu validasi BTA')
                                 console.log(res.data);
                                 navigation.goBack()
                             })
